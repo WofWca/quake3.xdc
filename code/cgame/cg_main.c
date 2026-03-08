768eb8f30bd3b6da95b883c9048dfc0e0ee85486
WofWca <wofwca@protonmail.com>
2026-03-08 16:04:45 +0400

    fix: clients not reading `sv_fps` from server
    This bug doesn't affect the original Quake III Arena source code,
    but only ioquake3 and its forks which have the fix for the
    "Files and cvars overwriting" security issue applied. See
    - https://github.com/ioquake/ioq3/commit/7d51d75b05a9593508040162709043516c0f2a17.
    - https://github.com/ioquake/ioq3/commit/fb34e78b7e340304ea1d5f6cee48e4dba8b731a5.
    - https://aluigi.altervista.org/adv/q3cfilevar-adv.txt.
    
    The solution is to properly mark the variable as CVAR_SYSTEMINFO,
    as in fb34e78.
    
    Also note that the bug doesn't manifest itself
    if the server has `sv_fps 20`,
    because 20 is the value that clients default to.

--- code/cgame/cg_main.c
+++ code/cgame/cg_main.c
@@ -2,6 +2,7 @@
 ===========================================================================
 Copyright (C) 1999-2005 Id Software, Inc.
 Some portions Copyright (C) 2006 Neil Toronto.
+Some portions Copyright (C) 2025 WofWca.
 
 This file is part of Quake III Arena source code.
 
@@ -340,7 +341,7 @@ static cvarTable_t cvarTable[] = { // bk001129
 	{ &cg_drawBBox, "cg_drawBBox", "0", CVAR_CHEAT },
 	{ &cg_cmdTimeNudge, "cg_cmdTimeNudge", "0", CVAR_ARCHIVE | CVAR_USERINFO },
 	// this will be automagically copied from the server
-	{ &sv_fps, "sv_fps", "20", 0 },
+	{ &sv_fps, "sv_fps", "20", CVAR_SYSTEMINFO },
 	{ &cg_projectileNudge, "cg_projectileNudge", "0", CVAR_ARCHIVE },
 	{ &cg_optimizePrediction, "cg_optimizePrediction", "1", CVAR_ARCHIVE },
 	{ &cl_timeNudge, "cl_timeNudge", "0", CVAR_ARCHIVE },
